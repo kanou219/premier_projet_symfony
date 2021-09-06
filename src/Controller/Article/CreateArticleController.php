b695e9d2d58a549a314dd4db45d3f6045dd3512d
Tandavarayen <luc.tandavarayen@gmail.com>
2021-09-06 17:11:09 +0200

    formulaire

---
+++ src/Controller/Article/CreateArticleController.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Controller\Article;
+
+use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+
+class CreateArticleController extends AbstractController
+
+{
+
+    /**
+     * @route("/article/creer",name="creer_article")
+     */
+    public function create()
+
+    {
+        return $this ->render("article/creer.html.twig");
+    }
+}
